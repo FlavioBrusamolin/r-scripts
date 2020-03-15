e4779024cfa22190a7a5280191ed7c21df3ae109
Flavio Brusamolin Brito <flaviobrusamolin@gec.inatel.br>
2020-03-15 14:49:10 -0300

    R scripts

---
+++ scripts/flavio_leonardo.R
@@ -0,0 +1,22 @@
+# Questao 1
+var1 = 50;
+var2 = 12;
+
+result = var1 %% var2
+result
+
+#Questao 2
+vector = c(0:29)
+vector
+
+#Questao 3
+matriz = matrix(seq(from = 10, to = 100, by = 10), nr = 5, nc = 2, byrow = T)
+matriz
+
+#Questao 4
+vector2 = as.vector(matriz)
+vector2
+
+#Questao 5
+list1 = list(vector, matriz)
+list1
