ec6bb3f71637f7a03668d6c977d46fabd55a26cf
Flavio Brusamolin Brito <flaviobrusamolin@gec.inatel.br>
2020-03-15 20:02:26 -0300

    Questions 6 ~ 9

--- scripts/flavio_leonardo.R
+++ scripts/flavio_leonardo.R
@@ -1,22 +1,42 @@
-# Questao 1
+#Question 1
 var1 = 50;
 var2 = 12;
 
 result = var1 %% var2
 result
 
-#Questao 2
+#Question 2
 vector = c(0:29)
 vector
 
-#Questao 3
-matriz = matrix(seq(from = 10, to = 100, by = 10), nr = 5, nc = 2, byrow = T)
-matriz
+#Question 3
+matrix1 = matrix(seq(from = 10, to = 100, by = 10), nr = 5, nc = 2, byrow = T)
+matrix1
 
-#Questao 4
+#Question 4
 vector2 = as.vector(matriz)
 vector2
 
-#Questao 5
+#Question 5
 list1 = list(vector, matriz)
 list1
+
+#Question 6
+names = c("Leonardo", "Flavio", "Bruno", "Danilo")
+ages = c(22, 2, 21, 22)
+name_x_age = cbind(names, ages)
+
+#Question 7
+iris
+str(iris)
+
+#Question 8
+getwd()
+table = read.table("effort.dat", TRUE, sep=",", quote="\"")
+
+#Question 9
+table = read.table("effort.dat", TRUE, sep=",", quote="\"")
+new_dataframe = table
+names(new_dataframe)[1] <- "Configuração Esforço Mudança"
+
+#Question 10
